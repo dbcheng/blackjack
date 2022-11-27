904b5c14f637fe431b7dc511e6a2c2682136f99d
David Cheng <davidbcheng@gmail.com>
2022-11-27 11:49:12 -0500

    Add Deck

--- main/blackjack.cc
+++ main/blackjack.cc
@@ -1,16 +1,33 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include "deck.h"
 
 
-struct Card {
-    
-};
+
 
 int main(void) {
-    std::cout << "blackjack compiling with C++ version: "<< __cplusplus << std::endl;
-    std::vector<std::string> v = {"foo", "bar", "baz"};
-    std::cout << "v: "<< v[0] << std::endl;
+    std::cout << "blackjack compiling with C++ version: "<< __cplusplus <<
+        std::endl;
+
+    std::string cmd;
+    std::cout << "Start Playing? (y/n): ";
+    std::cin >> cmd;
+    while (cmd != "n") {
+        // Do hand
+        //
+        
+        std::cout << "Start Next Hand? (y/n): ";
+        std::cin >> cmd;
+    }
+    
+    Deck deck;
+    
+    while (deck.Size()) {
+        std::cout << deck.Next().Display() << std::endl;
+    }
+
+
 
     return 0;
 }
---
+++ main/deck.h
@@ -0,0 +1,92 @@
+#include <string>
+#include <sstream>
+#include <cassert>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <random>
+
+
+std::map<std::string, int> kRanks {
+    {"Two", 2},
+    {"Three", 3},
+    {"Four", 4},
+    {"Five", 5},
+    {"Six", 6},
+    {"Seven", 7},
+    {"Eight", 8},
+    {"Nine", 9},
+    {"Ten", 10},
+    {"Jack", 10},
+    {"Queen", 10},
+    {"King", 10},
+    {"Ace", 11}
+    // TODO: Add Jokers
+};
+
+std::vector<std::string> kSuits {
+    "Club",
+    "Diamond",
+    "Heart",
+    "Spade"
+};
+
+struct Card {
+    std::string rank;   
+    int value;
+    std::string suit;
+
+    std::string Display() {
+        std::stringstream s;
+        s << rank << " " << suit;
+        return s.str();
+    }
+};
+
+class Deck {
+ public:
+     Deck() {
+         for (const auto& [rank, value] : kRanks) {
+             for (const std::string& suit : kSuits) {
+                 cards_.push_back(Card{.rank=rank, .value=value, .suit=suit});
+             }
+         }
+        auto rng = std::default_random_engine {};
+        std::shuffle(std::begin(cards_), std::end(cards_), rng);
+     };
+
+     int Size() {
+         return cards_.size();
+     }
+
+     // Up to the caller to check cards size?
+     Card Next() {
+         assert(!cards_.empty());
+         Card next_card = cards_[0];
+         cards_.erase(cards_.begin());
+         used_cards_.push_back(next_card);
+         return next_card;
+     };
+
+     void Reset() {
+         for (const Card& card : used_cards_) {
+             cards_.push_back(card);
+         }
+         used_cards_.clear();
+         assert(used_cards_.empty());
+         assert(cards_.size() == 52);
+     }
+
+     std::string Display() {
+        std::stringstream result;
+        for (int i = 0; i < cards_.size(); i++) {
+            result << "Card " << i+1 << ": "<< cards_[i].Display() << std::endl;
+        }
+        return result.str();
+     }
+
+ private:
+     std::vector<Card> cards_;
+     std::vector<Card> used_cards_;
+
+};
